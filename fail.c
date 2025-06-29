679ea8c532d8c806d37167c5eefd2125b4088ba1
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-06-29 22:20:57 +0200

    fail: add debug mode

--- fail.c
+++ fail.c
@@ -4,8 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
 void fail(char *fmt, ...) {
   va_list ap;
+  if (DEBUG)
+    assert(0);
   fputs("error: ", stderr);
   va_start(ap, fmt);
   vfprintf(stderr, fmt, ap);
