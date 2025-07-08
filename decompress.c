0618160da7e9ff61a7f39f4ddb42c6b8751b1ef5
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-07-08 19:29:18 +0200

    Refactor chunk handling

--- decompress.c
+++ decompress.c
@@ -44,30 +44,66 @@ struct chunk {
 
 int nchunk;
 
+void findchunks(unsigned char *data, int size) {
+  unsigned char *end = data + size;
+  while (data < end) {
+    struct chunk *ch = chunk + nchunk++;
+    if (nchunk == nelem(chunk))
+      fail("too many chunks");
+    ch->ID = readsint(data, 32);
+    ch->size = readsint(data + 4, 32);
+    if (ch->size < 0)
+      fail("chunk %4.4s has negative size %d", data, ch->size);
+    ch->data = data + 8;
+    data += 8 + ch->size;
+  }
+  if (data != end)
+    fail("sum of chunks %lld larger than FORM", data - end);
+}
+
+struct chunk *getchunk(int32_t ID) {
+  unsigned char idbuf[4];
+  struct chunk *ch, *out = 0;
+  putbe(ID, 32, idbuf);
+  for (ch = chunk; ch < chunk + nchunk; ch++) {
+    if (ch->ID == ID) {
+      if (out)
+        fail("more than one %s chunk", idbuf);
+      else
+        out = ch;
+    }
+  }
+  if (!out)
+    fail("missing %s", idbuf);
+  return out;
+}
+
 unsigned char commbuf[38];
 
 int main(int argc, char **argv) {
   int nchannels, wordsize, infd, outfd, i;
   struct chunk *ch;
-  unsigned char *in, *p, *informend, *out, *ssndstart;
+  unsigned char *in, *p, *out, *ssndstart;
   uint16_t outwordsize = 0;
   uint32_t nsamples;
   int32_t formsize;
   struct chunk *comm = 0, *ssnd = 0, ssndcompressed;
   struct stat st;
+
   if (argc != 3) {
     fputs("Usage: decompress INFILE OUTFILE\n", stderr);
     exit(1);
   }
   if (infd = open(argv[1], O_RDONLY), infd < 0)
     failerrno("cannot open %s", argv[1]);
+  if (outfd = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644), outfd < 0)
+    failerrno("open: %s", argv[2]);
+
   if (fstat(infd, &st))
     failerrno("fstat %s", argv[1]);
   if (in = mmap(0, st.st_size, PROT_READ, MAP_SHARED, infd, 0),
       in == MAP_FAILED)
     failerrno("mmap %s", argv[1]);
-  if (outfd = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644), outfd < 0)
-    failerrno("open: %s", argv[2]);
   p = in;
   if (readsint(p, 32) != 'FORM' || readsint(p + 8, 32) != 'AIFC')
     fail("invalid header: %12.12s", p);
@@ -75,34 +111,9 @@ int main(int argc, char **argv) {
   if (formsize < 0)
     fail("negative FORM size: %d\n", formsize);
   p += 12;
-  informend = in + 8 + formsize;
+  findchunks(p, formsize - 4);
 
-  while (p < informend) {
-    ch = chunk + nchunk++;
-    if (nchunk == nelem(chunk))
-      fail("too many chunks");
-    ch->ID = readsint(p, 32);
-    ch->size = readsint(p + 4, 32);
-    if (ch->size < 0)
-      fail("chunk %4.4s has negative size %d", p, ch->size);
-    ch->data = p + 8;
-    if (ch->ID == 'COMM') {
-      if (comm)
-        fail("more than one COMM");
-      comm = ch;
-    } else if (ch->ID == 'SSND') {
-      if (ssnd)
-        fail("more than one SSND");
-      ssnd = ch;
-    }
-    p += 8 + ch->size;
-  }
-  if (p != informend)
-    fail("sum of chunks %lld larger than FORM", p - informend);
-  if (!comm)
-    fail("missing COMM");
-  if (!ssnd)
-    fail("missing SSND");
+  comm = getchunk('COMM');
   if (comm->size < 18)
     fail("COMM too small: %d", comm->size);
   memmove(commbuf, comm->data, 18);
@@ -121,8 +132,11 @@ int main(int argc, char **argv) {
   outwordsize = (wordsize + 7) & ~7;
   comm->data[6] = outwordsize >> 8;
   comm->data[7] = outwordsize;
+
+  ssnd = getchunk('SSND');
   ssndcompressed = *ssnd;
   ssnd->size = 8 + nsamples * nchannels * outwordsize / 8;
+
   for (ch = chunk, formsize = 4; ch < chunk + nchunk; ch++)
     formsize += ch->size;
   if (ftruncate(outfd, formsize + 8))
@@ -143,6 +157,7 @@ int main(int argc, char **argv) {
       memmove(p, ch->data, ch->size);
     p += ch->size;
   }
+
   putbe(0, 32, ssndstart);
   putbe(0, 32, ssndstart + 4);
   ssndcompressed.data += 8;
