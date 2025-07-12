50850e1aab6345a62d533177c83c01cba8b22183
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-07-12 11:32:53 +0200

    compress: move file type check

--- compress.c
+++ compress.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
-  unsigned char buf[12], *in;
+  unsigned char buf[8], *in;
   int32_t filetype, formsize;
   FILE *f;
   if (argc != 3) {
@@ -16,20 +16,20 @@ int main(int argc, char **argv) {
   f = fopen(argv[1], "r");
   if (!f)
     failerrno("open %s", argv[1]);
-  if (fread(buf, 1, sizeof(buf), f) != sizeof(buf))
+  if (!fread(buf, sizeof(buf), 1, f))
     fail("read AIFF header: short read");
   if (readsint(buf, 32) != 'FORM')
     fail("missing FORM");
   formsize = readsint(buf + 4, 32);
-  if (formsize < 0)
+  if (formsize < 4 || formsize > INT32_MAX - 8)
     fail("invalid FORM size: %d", formsize);
-  filetype = readsint(buf + 8, 32);
-  if (filetype != 'AIFF' && filetype != 'AIFC')
-    fail("invalid file type: %4.4s", buf + 8);
   in = malloc(formsize + 8);
   if (!in)
     fail("malloc failed");
   memmove(in, buf, sizeof(buf));
-  if (fread(in, formsize - 4, 1, f) != 1)
+  if (!fread(in + sizeof(buf), formsize, 1, f))
     fail("short read from %s", argv[1]);
+  filetype = readsint(in + 8, 32);
+  if (filetype != 'AIFF' && filetype != 'AIFC')
+    fail("invalid file type: %4.4s", in + 8);
 }
