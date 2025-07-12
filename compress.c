d84fe4bacb64255122e5b456df89d0c7ae81fe3e
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-07-12 21:07:51 +0200

    Start writing output file

--- compress.c
+++ compress.c
@@ -28,34 +28,67 @@ unsigned char *finduniquechunk(int32_t ID, unsigned char *start,
   return chunk;
 }
 
-int main(void) {
-  unsigned char buf[8], *in, *inend, *comm, *ssnd, *out;
-  int32_t filetype, formsize, insize;
-  if (!fread(buf, sizeof(buf), 1, stdin))
+unsigned char *loadform(FILE *f, int32_t *size) {
+  unsigned char buf[8], *p;
+  int32_t formsize;
+  if (!fread(buf, sizeof(buf), 1, f))
     fail("read AIFF header: short read");
   if (readint(buf, 32) != 'FORM')
     fail("missing FORM");
   formsize = readint(buf + 4, 32);
   if (formsize < 4 || formsize > INT32_MAX - 8)
     fail("invalid FORM size: %d", formsize);
-  insize = formsize + 8;
+  *size = formsize + 8;
+  if (p = malloc(*size), !p)
+    fail("malloc input failed");
+  memmove(p, buf, sizeof(buf));
+  if (!fread(p + sizeof(buf), formsize, 1, f))
+    fail("short read");
+  return p;
+}
+
+int main(void) {
+  unsigned char *in, *inend, *comm, *ssnd, *out, *p, *q;
+  int32_t filetype, insize, commsize;
+  in = loadform(stdin, &insize);
   if (insize > INT32_MAX / 2)
     fail("input file too large");
-  if (in = malloc(insize), !in)
-    fail("malloc input failed");
-  memmove(in, buf, sizeof(buf));
-  if (!fread(in + sizeof(buf), formsize, 1, stdin))
-    fail("short read");
   inend = in + insize;
   filetype = readint(in + 8, 32);
   if (filetype != 'AIFF' && filetype != 'AIFC')
     fail("invalid file type: %4.4s", in + 8);
+  findchunk(0, in + 12, inend); /* validate chunk sizes */
   if (comm = finduniquechunk('COMM', in + 12, inend), comm == inend)
     fail("cannot find COMM chunk");
+  if (commsize = readint(comm + 4, 32), commsize < 22)
+    fail("COMM chunk too small: %d", commsize);
   if (filetype == 'AIFC' && readint(comm + 8 + 18, 32) != 'NONE')
     fail("unsupported input audio format: %4.4s", comm + 8 + 18);
   if (ssnd = finduniquechunk('SSND', in + 12, inend), ssnd == inend)
     fail("cannot find SSND chunk");
   if (out = malloc(2 * insize), !out)
     fail("malloc output failed");
+  p = in + 12;
+  q = out + 12;
+  while (p < inend - 8) {
+    int32_t ID = readint(p, 32), size = readint(p + 4, 32);
+    if (ID == 'COMM') {
+      int compressoff = 18 + 8;
+      memmove(q, p, compressoff);
+      memmove(q + compressoff, "DWVWxDelta With Variable Word Width\x00", 36);
+      q[compressoff + 4] =
+          0x1f; /* not allowed to put \x1f in string literal?? */
+      putbe(18 + 36, 32, q + 4);
+      q += 18 + 36 + 8;
+    } else if (ID == 'SSND') {
+    } else {
+      memmove(q, p, size + 8);
+      q += size + 8;
+    }
+    p += size + 8;
+  }
+  putbe('FORM', 32, out);
+  putbe(q - out - 8, 32, out + 4);
+  putbe('AIFC', 32, out + 8);
+  fwrite(out, q - out, 1, stdout);
 }
